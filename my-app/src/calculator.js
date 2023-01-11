b6c78008fe7ceb2d894e8e97da31955559c71b19
RamPakanayev <rampakanayev@gmail.com>
2023-01-11 13:05:10 +0200

    lesson 7
    creating a calculator component

---
+++ my-app/src/calculator.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function devide(a, b) {
+  return a / b;
+}
+
+//export default add;
+export { add, sub, multiply, devide };
